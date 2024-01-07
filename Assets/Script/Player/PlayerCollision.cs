ef9162969afb31aeeaf4bb99f80c45325956a4b5
Noval ian <andrianau26@gmail.com>
2024-01-07 18:16:31 +0800

    code

--- Assets/Script/Player/PlayerCollision.cs
+++ Assets/Script/Player/PlayerCollision.cs
@@ -1,40 +1,39 @@
-using System;
-using System.Collections;
 using UnityEngine;
+using System.Collections;
+
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             HealthManager.health--;
-            Debug.Log(HealthManager.health);
-            Console.WriteLine(HealthManager.health);
+            Debug.Log("Health: " + HealthManager.health);
+
             if (HealthManager.health <= 0)
             {
                 Invoke("RestartGame", 0.5f);
             }
             else
             {
-                StartCoroutine(getDamage());
+                StartCoroutine(GetDamageRoutine());
             }
         }
-
     }
 
-    // THIS METHOD FOR INVISIBILITY FOR FEW SECONDS 
-    IEnumerator getDamage()
+    IEnumerator GetDamageRoutine()
     {
-        Physics2D.IgnoreLayerCollision(6, 9);
-        yield return new WaitForSeconds(3);
-        Physics2D.IgnoreLayerCollision(6, 9, false);
-        Movement.isSprinting = true ;
-        yield return new WaitForSeconds(1.2f);
-        Movement.isSprinting = false;
+        Physics2D.IgnoreLayerCollision(6, 9, true);
+        Movement.isSprinting = true;
+        yield return new WaitForSeconds(3f);
 
+        Physics2D.IgnoreLayerCollision(6, 9, false);
+        Movement.isSprinting = false;
+    }
+
+    // Metode untuk me-restart game
+    private void RestartGame()
+    {
+        Debug.Log("Game Restarted");
     }
 }
